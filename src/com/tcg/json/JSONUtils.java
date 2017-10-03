b31f7ffe0f1320949e057a4569c2f41a5586836e
TruckLin <32349155+TruckLin@users.noreply.github.com>
2017-10-03 17:37:50 +0800

    2017/10/03
    /src/con/tcg/json contains helper functions : FileHandle.java, JSONUtils.java. 
    
    /assets:    currently contains some JSON files for testing purpose.
    
    /lib:    json.jar: .jar file of JSON-Java package. updated on 20170516.

---
+++ src/com/tcg/json/JSONUtils.java
@@ -0,0 +1,85 @@
+package com.tcg.json;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.json.JSONObject;
+
+public class JSONUtils {
+	
+	public static String getJSONStringFromFile(String path) {
+		Scanner scanner;
+		InputStream in = FileHandle.inputStreamFromFile(path);
+		scanner = new Scanner(in);
+		String json = scanner.useDelimiter("\\z").next();
+		
+		//Testing
+		//System.out.println("Text we get from getJSONStringFromFile() : \n"+json);
+		
+		scanner.close();
+		try {
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return json;
+	}
+	
+	public static JSONObject getJSONObjectFromFile(String path) {
+		return new JSONObject(getJSONStringFromFile(path));
+	}
+	
+	// This function should allow us to return multiple JSONOnjects from the same file.
+	public static JSONObject[] getMultipleJSONObjectsFromFile(String path) {
+		JSONObject[] Objs;
+		ArrayList<String> ObjStrings = new ArrayList<String>();
+		
+		String json = getJSONStringFromFile(path);
+		
+		int isObj = 0;
+		int StartIdx = 0;
+		for(int i = 0; i<json.length();i++) {
+			if( json.charAt(i) == '{'  && isObj == 0) {
+				isObj++;
+				StartIdx = i;
+			}else if(json.charAt(i) == '{') {
+				isObj++;
+			}else if(json.charAt(i) == '}') {
+				isObj--;
+			}else {}
+			
+			if(isObj==0 && json.charAt(i) == '}') {
+				ObjStrings.add(json.substring(StartIdx, i+1));
+				StartIdx = i+1;
+			}
+		}
+		
+		Objs = new JSONObject[ObjStrings.size()];
+		for(int i=0; i < ObjStrings.size() ; i++) {
+			//Testing
+			//System.out.println(i+"th String = \n");
+			//System.out.println(ObjStrings.get(i));
+			
+			Objs[i] = new JSONObject(ObjStrings.get(i));
+		}
+		
+		return Objs;
+	}
+	
+	public static boolean bjectExists(JSONObject jsonObject, String key) {
+		Object o;
+		
+		try {
+			o = jsonObject.get(key);
+		}catch(Exception e) {
+			return false;
+		}
+		
+		return o != null;
+	}
+	
+
+}
